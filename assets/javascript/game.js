eed0d476ddc5625605e09190d579046ea42fcc0b
Adrienne <iliu.adrienne@gmail.com>
2019-05-25 18:13:49 -0700

    add outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//variables
+
+// Reset function - Player will see computer's generated guess but everything else is clear
+// When game restarts after a win/lose, the computer+food-buttons will generate a new number and win/lose/score counter will go to zero
+
+//Each food-button will generate a random number that stays consistant until the player wins/loses the game. 
+
+//pressing the food-buton, the score will display the first value and keep increasing depending on the button's value 
+
+//function for recognizing if the score is equal to or higher than computer's guess
+//
+
